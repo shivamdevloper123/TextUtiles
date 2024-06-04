494c1e0f49e9b02800d7c55b127256a7064f8e40
shivamdevloper123 <shivam71066@gmail.com>
2024-06-04 23:30:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Navbar from './componants/Navbar';
+
+function App() {
+  return (
+    <>
+  <Navbar  title="TextUtlis" homeText="Home 1"  aboutText="About" />
+    </>
+  );
+}
+
+export default App;
